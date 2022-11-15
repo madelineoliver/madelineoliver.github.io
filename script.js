43ea833c3ec179409ea588245fb8088d4d003dc9
KrisZuniga <112186297+KrisZuniga@users.noreply.github.com>
2022-11-14 21:32:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+d3.csv("Top 10 Albums By Year Album Length-Sheet1.csv").then(function (data){
+
+    var size = d3.min([window.innerWidth*0.9, window.innerHeight*0.9])
+
+    var dimensions = ({
+            width: size, 
+            height: size/3,
+            margin: {
+            top: 10,
+            right: 10,
+            bottom: 50,
+            left: 50
+        }
+    })
+})
