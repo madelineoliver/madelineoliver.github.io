bd4fb4ef7b15f469c137c4232ca7e197de6d0214
Kristina Zuniga <kzuniga@g.clemson.com>
2022-11-21 18:19:30 -0500

    <new>

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ d3.csv("Top 10 Albums By Year Album Length-Sheet1.csv").then(function (data){
         }
     })
 
-    var svg = d3.select(#vis1)
+    var svg = d3.select("#vis1")
         .style("width", dimensions.width)
         .style("height", dimensions.height)
 
